b7e1ba4aaf63c2b5dce3924a2a23b5ed4573e588
birgirg20 <113178302+birgirg20@users.noreply.github.com>
2023-03-18 11:59:41 +0000

    eslint rule, no-moment

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+    rules: {
+      "no-restricted-imports": [
+        "error",
+        {
+          "paths": [{
+            "name": "moment",
+            "message": "Use date-fns instead."
+          }]
+        }
+      ]
+    }
+  };
